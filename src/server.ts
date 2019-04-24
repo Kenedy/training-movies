a8e2d37afee745a997af6327ccf3ec13e44a683c
Kenedy <radek.skoupy@solitea.cz>
2019-04-24 12:15:20 +0200

    express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log('hic sunt leones');
+import express = require('express');
+const app = express();
+
+// CORS
+app.use(function(_req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-CSRFToken');
+    res.header('Access-Control-Expose-Headers', 'X-CSRFToken');
+    next();
+  });
+
+app.get('/', (_req, res) => res.send('hic sunt leones') );
+
+app.listen(8080, () => console.log('training-movies backend listening on port 8080'));
